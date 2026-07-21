84f6b1f88213520ba31c0e2b6f93d1808e0edd95
iva <ivallen84@gmail.com>
2026-07-21 17:47:11 -0600

    Se crea el paquete jugador y sus clases
    se crea la clase jugador y controlJugadores

---
+++ src/jugadores/Jugador.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jugadores;
+
+/**
+ *
+ * @author gabri
+ */
+public class Jugador {
+    protected String name;
+    protected int puntaje;
+    protected int parejas;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public int getParejas() {
+        return parejas;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public void setParejas(int parejas) {
+        this.parejas = parejas;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/jugadores/controlJugadores.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jugadores;
+
+/**
+ *
+ * @author gabri
+ */
+public class controlJugadores {
+    private Jugador modelo;
+    private int contPareja;
+
+    public controlJugadores(Jugador modelo) {
+        this.modelo = modelo;
+        this.contPareja = 0;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
