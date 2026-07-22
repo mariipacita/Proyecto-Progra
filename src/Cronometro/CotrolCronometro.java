d920a4a3c803094a5ed7de6f5f6fa81224ad8f81
iva <ivallen84@gmail.com>
2026-07-21 19:34:10 -0600

    Clase crontrolador del cronometro
    se crean metodosdentro de la clase

--- src/Cronometro/CotrolCronometro.java
+++ src/Cronometro/CotrolCronometro.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Cronometro;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.Timer;
 /**
  *
@@ -14,12 +16,109 @@ private cronometro cronometro;
     
     private Timer timer;
 
-public void temporizador(){
+    public CotrolCronometro(cronometro cronometro, Timer timer) {
+        this.cronometro =new cronometro();
+        this.timer = new Timer(1000, new ActionListener(){
+             @Override
+    public void actionPerformed(ActionEvent e) {
+      }
+     });
+    }
     
-}
+    public void iniciar(){
+        timer.start();
+    }
+    public void parar(){
+        timer.stop();
+    }
+    
+    public void regresarTiempo(){
+        timer.stop();
+        cronometro.reinicioCronometro();
+    }
+    public int timepoObtenido(){
+        return cronometro.getTime();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
 
 
 
 
-}
+
     
