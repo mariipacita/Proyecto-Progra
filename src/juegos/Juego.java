b4a15e7fe5f173a6aafb6dec83ffadb8c9ea55d5
Sharys29 <sharysceciliano@gmail.com>
2026-07-24 00:38:05 -0600

    Clase Juegos
    se creo la clase juegos

---
+++ src/juegos/Juego.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegos;
+
+import Cronometro.cronometro;
+import jugadores.Jugador;
+
+/**
+ *
+ * @author sharys
+ */
+public class Juego {
+    private Tablero tablero;
+    private Jugador jugador;
+    private cronometro Cronometro;
+    private Nivel nivel;
+    
+    
+    public Juego(Jugador jugador, Nivel nivel){
+        this.jugador = jugador;
+        this.nivel = nivel;
+        this.tablero = new Tablero(nivel);
+        this.Cronometro = new cronometro();
+    }
+    
+    public void iniciarPartida() {
+
+}
+
+public void seleccionarCarta(int fila, int columna) {
+
+}
+
+public void verificarPareja() {
+
+}
+
+public void actualizarJugador() {
+
+}
+
+public void finalizarJuego() {
+
+}
+
+public void reiniciarPartida() {
+
+}
+}
+    
