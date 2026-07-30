8a59cd5903d832b772cb78f66e3b6fea88044a92
Sharys29 <sharysceciliano@gmail.com>
2026-07-29 20:23:48 -0600

    ControlJuego
     se cambio el controlador de paquete

--- src/ControladorJuegoMemoria/ControlJuego.java
+++ src/ControladorJuegoMemoria/ControlJuego.java
@@ -2,9 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package juegos;
+package ControladorJuegoMemoria;
 
-import juegos.Juego;
+
+    import juegos.Juego;
 
 /**
  *
@@ -41,3 +42,5 @@ public class ControlJuego {
         return juego;
     }
 }
+
+
