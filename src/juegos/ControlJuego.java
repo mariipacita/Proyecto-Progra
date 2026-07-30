cbf6c93b12e85f8111f2f17de5e02887723284bc
Sharys29 <sharysceciliano@gmail.com>
2026-07-29 20:05:09 -0600

    ControlJuego
    Se realizo el controlador de la clase Juego

---
+++ src/juegos/ControlJuego.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegos;
+
+import juegos.Juego;
+
+/**
+ *
+ * @author sharys
+ */
+public class ControlJuego {
+    private Juego juego;
+    
+    public ControlJuego(Juego juego){
+        this.juego = juego;
+    }
+    
+    public void iniciarPartida() {
+        juego.iniciarPartida();
+    }        
+    
+    public void seleccionarCarta(int fila, int columna){
+        juego.seleccionarCarta(fila, columna);
+    }
+    
+    public void actualizarJugador(){
+        juego.actualizarJugador();
+    }
+    
+    public void finalizarJuego(){
+        juego.finalizarJuego();
+    }
+    
+    public void reiniciarPartida(){
+        juego.reiniciarPartida();
+    }
+    
+    public Juego getJuego(){
+        return juego;
+    }
+}
